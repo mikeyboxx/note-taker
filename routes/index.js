60f10f303762a5f5681a28ee756887c0a09f0cc1
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-27 18:42:21 -0400

    Added routes for HTML requests, and updated main router index.js.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html');
 
-
-
+router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
+
+
+// HTML routes
+//  GET :  / - returns index.html
+//  GET :  /notes - returns notes.html
+
+// API routes
+// GET     -  /api/notes/  - returns an array
+// POST    - /api/notes/
+// DELETE  - /api/notes/:id
+
--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express(); // star server
-
-const routes = require('./routes');
+const PORT = process.env.PORT || 3001;   // get port from environment, otherwise default to 3001
+const app = express();   // start server
+const routes = require('./routes');  // import application routes
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(routes);
 
-
-
+app.use(routes);  // all of the routes are located in one place
 
 // server is up and running
 app.listen(PORT, ()=>{

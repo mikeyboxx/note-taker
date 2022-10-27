66daf7641da426ab639e3f9d612a5c37e910a18c
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-27 16:28:41 -0400

    Added routes folder with index.js, and changed server.js to use that middleware.

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express(); // star server
 
+const routes = require('./routes');
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use(routes);
 
 
 

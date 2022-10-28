20cb64c6a0b286d7ba6e5c47a6368fcf1da44e8c
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-27 21:20:32 -0400

    Added deleteNotes.js controller

---
+++ controllers/deleteNote.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const notes = require('../db/db.json');
+
+const deleteNote = (req,res)=>{
+    console.info(`${req.method} request delete a note`);
+
+    if (req.params.id) {
+        const idx = notes.findIndex(el=> el.id === req.params.id);
+        if (idx === -1){
+            res.status(500).json('Error in posting note. Id to be deleted is not found.');
+        } else {
+            const response = {
+                status: 'success',
+                type: 'DELETE',
+                data: notes[idx],
+            };
+            notes.splice(idx,1);
+            // save array as json string, in db.json file  
+             fs.writeFile('./db/db.json', JSON.stringify(notes, null, 4), (err) => {
+                if (err){
+                    console.error(err);
+                    res.status(500).send(err.message);
+                } else {
+                    console.info(`\nId: ${req.params.id} has been deleted from ./db/db.json`);  
+                    res.status(201).send(response); // return our own response object
+                }
+            })
+        }
+    }
+};
+
+module.exports = deleteNote;
--- routes/api/notes.js
+++ routes/api/notes.js
@@ -3,6 +3,7 @@ const {v4: uuidv4} = require('uuid');
 const fs = require('fs');
 const notes = require('../../db/db.json');
 const postNote = require('../../controllers/postNote.js');
+const deleteNote = require('../../controllers/deleteNote.js');
 
 // GET request to get all notes
 router.get('/', (req,res)=>{
@@ -13,33 +14,7 @@ router.get('/', (req,res)=>{
 // POST request to add a note
 router.post('/', postNote);
 
-
-router.delete('/:id', (req,res)=>{
-    console.info(`${req.method} request delete a note`);
-
-    if (req.params.id) {
-        const idx = notes.findIndex(el=> el.id === req.params.id);
-        if (idx === -1){
-            res.status(500).json('Error in posting note. Id to be deleted is not found.');
-        } else {
-            const response = {
-                status: 'success',
-                type: 'DELETE',
-                data: notes[idx],
-            };
-            notes.splice(idx,1);
-            // save array as json string, in db.json file  
-             fs.writeFile('./db/db.json', JSON.stringify(notes, null, 4), (err) => {
-                if (err){
-                    console.error(err);
-                    res.status(500).send(err.message);
-                } else {
-                    console.info(`\nId: ${req.params.id} has been deleted from ./db/db.json`);  
-                    res.status(201).send(response); // return our own response object
-                }
-            })
-        }
-    }
-});
+// DELETE request to delete a note
+router.delete('/:id', deleteNote);
 
 module.exports = router;

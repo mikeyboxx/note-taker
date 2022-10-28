f4a146a71364bbe0d3cb0657791bc23589fbc890
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-27 20:33:03 -0400

    Changed notes.js router for /api/notes resource, to add delete method to delete note from global array, and save to db.json file

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -32,6 +32,7 @@ router.post('/', (req,res)=>{
         // construct our own response object 
         const response = {
             status: 'success',
+            type: 'ADD',
             data: newNote,
         };
 
@@ -47,6 +48,35 @@ router.post('/', (req,res)=>{
         });
     } else {
         res.status(500).json('Error in posting note');
+    }
+});
+
+
+router.delete('/:id', (req,res)=>{
+    console.info(`${req.method} request delete a note`);
+
+    if (req.params.id) {
+        const idx = notes.findIndex(el=> el.id === req.params.id);
+        if (idx === -1){
+            res.status(500).json('Error in posting note. Id to be deleted is not found.');
+        } else {
+            const response = {
+                status: 'success',
+                type: 'DELETE',
+                data: notes[idx],
+            };
+            notes.splice(idx,1);
+            // save array as json string, in db.json file  
+             fs.writeFile('./db/db.json', JSON.stringify(notes, null, 4), (err) => {
+                if (err){
+                    console.error(err);
+                    res.status(500).send(err.message);
+                } else {
+                    console.info(`\nId: ${req.params.id} has been deleted from ./db/db.json`);  
+                    res.status(201).send(response); // return our own response object
+                }
+            })
+        }
     }
 });
 
